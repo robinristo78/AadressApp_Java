439ee1623103dcf1304e320dff3036428d2a54c7
robinristo78 <robin-risto.kivit@voco.ee>
2025-10-29 15:05:54 +0200

    4. pooleli praegu, in https://code.makery.ch/library/javafx-tutorial/part5/

--- src/main/java/ch/makery/address/MainApp.java
+++ src/main/java/ch/makery/address/MainApp.java
@@ -143,6 +143,44 @@ public class MainApp extends Application {
     }
 
     /**
+     * Returns the person file preference, i.e. the file that was last opened.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     *
+     * @return
+     */
+//    public File getPersonFilePath() {
+//        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+//        String filePath = prefs.get("filePath", null);
+//        if (filePath != null) {
+//            return new File(filePath);
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    /**
+//     * Sets the file path of the currently loaded file. The path is persisted in
+//     * the OS specific registry.
+//     *
+//     * @param file the file or null to remove the path
+//     */
+//    public void setPersonFilePath(File file) {
+//        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+//        if (file != null) {
+//            prefs.put("filePath", file.getPath());
+//
+//            // Update the stage title.
+//            primaryStage.setTitle("AddressApp - " + file.getName());
+//        } else {
+//            prefs.remove("filePath");
+//
+//            // Update the stage title.
+//            primaryStage.setTitle("AddressApp");
+//        }
+//    }
+
+    /**
      * Returns the main stage.
      * @return
      */
