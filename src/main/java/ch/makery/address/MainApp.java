c38eacd20f5fa1ddb636c86258526fdd2dcfa2cf
robinristo78 <robin-risto.kivit@voco.ee>
2025-10-29 14:29:59 +0200

    3. CSS Styling, in https://code.makery.ch/library/javafx-tutorial/part4/

--- src/main/java/ch/makery/address/MainApp.java
+++ src/main/java/ch/makery/address/MainApp.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -55,6 +56,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.getIcons().add(new Image("file:src/main/resources/images/address_book_32.png"));
 
         initRootLayout();
 
